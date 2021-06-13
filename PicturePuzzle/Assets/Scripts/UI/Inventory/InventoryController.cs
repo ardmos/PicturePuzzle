081e944dafac8cd2317f3028925299a443edc93e
ardmos <46891528+ardmos@users.noreply.github.com>
2021-06-13 22:41:49 +0900

    좋습니다,,, 주석을 참고해주세요

---
+++ PicturePuzzle/Assets/Scripts/UI/Inventory/InventoryController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ***인벤토리 컨트롤러***
+///   1. 인벤토리 버튼 클릭시 등장. (기본적으론 숨겨져있음)
+///   2. 아이템 드래그 or 바깥버튼 클릭시 퇴장.
+///   3. 아이템 드래그해서 배치. 성공시 폴라로이드에서 빠져나와 배치됨.
+/// </summary>
+
+public class InventoryController : MonoBehaviour
+{
+	public GameObject inventoryObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//기본적으로 숨겨두기.
+		inventoryObj.SetActive(false);
+    }
+
+    //아이템 만들 차례! 아이템 만들고, 여기 버튼들 기능 연결
+
+    public void OpenInventory()
+	{
+		inventoryObj.SetActive(true);
+	}
+    public void CloseInventory()
+	{
+		inventoryObj.SetActive(false);
+	}
+
+
+ 
+}
