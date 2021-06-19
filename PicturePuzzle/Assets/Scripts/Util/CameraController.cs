eb8ad01d44326cf8099fd334f8f7d0245dd9009c
ardmos <46891528+ardmos@users.noreply.github.com>
2021-06-20 01:33:29 +0900

    폴라로이드 확대 화면 드래그 기능 추가 완료.

--- PicturePuzzle/Assets/Scripts/Util/CameraController.cs
+++ PicturePuzzle/Assets/Scripts/Util/CameraController.cs
@@ -8,8 +8,8 @@ using UnityEngine.EventSystems;
 /// 1. 메인 카메라와 폴라로이드 카메라 화면 전환 기능
 /// 2. 화면 전환시 오디오 리스너도 전환.
 /// 3. 화면 전환시 UI세트도 전환.
-/// 4. 메인카메라일 시 화면 터치되는 위치로 폴라로이드카메라 영역 이동. ispolaroidCamera를 이용해 판단. 
-///
+/// 4. 메인카메라일 시 화면 터치되는 위치로 폴라로이드카메라 영역 이동. ispolaroidCamera를 이용해 판단. 드래그로도 이동 가능하도록 수정 0619
+/// 5. 확대된 상태에서도 움직일 수 있게 
 ///
 ///
 /// 카메라 촬영 기능 만들 차례임.  잠깐 이미지 넣고 옴!
@@ -26,7 +26,8 @@ public class CameraController : MonoBehaviour
     public GameObject mainUI, polaroidUI;
     //폴라로이드 에임 이미지 오브젝
     public GameObject polaroidAimObj;
-    // Start is called before the first frame update
+    //폴라로이드 확대 화면 이동을 위한 포지션변수
+    Vector3 beforePos = Vector3.zero, currentPos;
     void Start()
     {
         MainCameraON();
@@ -40,8 +41,14 @@ public class CameraController : MonoBehaviour
             //main카메라일때만.
             MovePolaroidAim();
         }
+        else
+        {
+            //Polaroid카메라일때. 화면 움직이기
+            MoveBigPolaroidAim();
+        }
     }
 
+    #region 메인-폴라로이드 카메라 전환
     public void ChangeCamera()
     {
         if (ispolaroidCamera)
@@ -53,7 +60,6 @@ public class CameraController : MonoBehaviour
             PolaroidCameraON();
         }
     }
-
     public void PolaroidCameraON()
     {
         //위치는 PolaroidAimObj의 위치. 단 z좌표는 -10 유지
@@ -79,10 +85,15 @@ public class CameraController : MonoBehaviour
         polaroidUI.SetActive(false);
         polaroidAimObj.SetActive(true);
     }
+    #endregion
 
+    #region 조그만 폴라로이드 에임 움직이기    
     public void MovePolaroidAim()
-    {                
-        if (Input.GetMouseButtonDown(0))
+    {
+        //폴라로이드 에임 이동. 
+
+        //if (Input.GetMouseButtonDown(0))  드래그시에도 따라 움직이게 하기 위해서 아래처럼 처리. 0619
+        if (Input.GetMouseButton(0))
         {
             //안드로이드인 경우에는 EventSystem.curren.IsPointerOverGameObject 이것의 처리를 다르게 해주자!
             if(Application.platform == RuntimePlatform.Android)
@@ -111,4 +122,35 @@ public class CameraController : MonoBehaviour
             }           
         }        
     }
+    #endregion
+
+    #region 확대된 폴라로이드 에임 움직이기
+    public void MoveBigPolaroidAim()
+    {
+        //Before-Current 만큼 이동시키기 
+
+        if (Input.GetMouseButton(0))
+        {
+            if (beforePos != Vector3.zero)
+            {
+                currentPos = polaroidCamera.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 polaroidCamPos = polaroidCamera.transform.position;
+                Vector3 movedis = (beforePos - currentPos);
+                polaroidCamera.transform.position = new Vector3(polaroidCamPos.x + movedis.x, polaroidCamPos.y + movedis.y, -10f);
+                //Debug.Log("beforePos:" + beforePos + ", currentPos:" + currentPos + ", movedis:" + movedis);
+            }
+            beforePos = polaroidCamera.ScreenToWorldPoint(Input.mousePosition);            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //한 번 손이 떼지고, 드래그가 끝나면 다음 드래그를 새롭게 시작할 때
+            //이전 드래그의 위치값이 관여하는일이 없도록 초기화 시켜준다. 
+            beforePos = Vector3.zero;
+            
+        }
+
+
+    }
+    #endregion
 }
