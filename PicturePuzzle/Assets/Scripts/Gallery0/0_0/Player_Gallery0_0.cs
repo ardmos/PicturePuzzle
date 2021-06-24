a5109d4994125fa89a42d9e1add8d811425d9834
ardmos <46891528+ardmos@users.noreply.github.com>
2021-06-24 23:29:17 +0900

    갤러리0_0 입장 퇴장 애니메이션추가, 갤러리0_1로 이동 버튼 추가.

---
+++ PicturePuzzle/Assets/Scripts/Gallery0/0_0/Player_Gallery0_0.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 0_0 플레이어 캐릭터 관리 스크립트.
+/// 
+/// 1. 등장, 퇴장 애니메이션 관리.
+/// 
+/// </summary>
+
+public class Player_Gallery0_0 : MonoBehaviour
+{
+    Animator animator;
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        //씬이 시작되면 자동으로 등장 애니메이션 호출.
+        StartEntryAnim();
+    }
+
+    public void StartEntryAnim()
+    {
+        //등장 애니메이션 실행
+        //Play on Awake로 자동 실행됨. 
+    }
+
+    public void StartExitAnim()
+    {
+        //퇴장 애니메이션 실행
+        //씬 이동 전에 캐릭터 애니메이션. 애니메이션이 끝나면 애니메이션 이벤트 컨트롤러로 씬 이동이 호출됨. 
+        animator.SetBool("Exit", true);
+    }
+
+    public void SceneTransferTo0_1()
+    {
+        //Exit애니메이션의 이벤트 컨트롤러로 호출됨.
+        FindObjectOfType<Galler0_0Manager>().To1From0();
+    }
+}
---
+++ PicturePuzzle/Assets/Scripts/Gallery0/Galler0_0Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Gallery 0_0과 0_1 모두를 총괄하는 매니저 스크립트.
+/// 
+/// 1. 각 그림들 클릭시 그림 씬으로 화면전환.
+/// 2. Gallery0_0과 0_1을 오가는 버튼의 관리.
+/// 
+/// </summary>
+
+public class Galler0_0Manager : MonoBehaviour
+{
+    #region 그림들 여는 버튼
+    public void Button_Squirrel()
+    {
+        //"Pic0_0_Squirrel"
+        SceneManager.LoadScene(2);
+    }
+    public void Button_Turtle()
+    {
+        //"Pic0_1_Turtle"
+        SceneManager.LoadScene(3);
+    }
+    public void Button_Stone()
+    {
+        //"Pic0_2_Stone"
+        SceneManager.LoadScene(4);
+    }
+    public void Button_Wood()
+    {
+        //"Pic0_3_Wood"
+        SceneManager.LoadScene(5);
+    }
+    #endregion
+
+    #region 갤러리0_0과 갤러리0_1의 화면 전환 버튼
+    public void To1From0()
+    {
+        //이동 애니메이션은 Player_Gallery0_0.cs에 있음. 
+        //0_0에서 0_1로 이동
+        SceneManager.LoadScene("Gallery0_1");
+    }
+    public void To0From1()
+    {
+        //이동 전에 캐릭터 애니메이션. 
+
+        //0_1에서 0_0으로 이동
+        SceneManager.LoadScene("Gallery0_0");
+    }
+    #endregion
+
+    
+}
