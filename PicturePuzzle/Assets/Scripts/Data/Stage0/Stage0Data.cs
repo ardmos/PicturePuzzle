72771d1f7cf0c8263528aca09712b02e260492fd
ardmos <46891528+ardmos@users.noreply.github.com>
2021-07-04 23:34:21 +0900

    ㅇㅋ 일단 이미지가 들어가긴 함. 사이즈 설정만 해주자!

--- PicturePuzzle/Assets/Scripts/Data/Stage0/Stage0Data.cs
+++ PicturePuzzle/Assets/Scripts/Data/Stage0/Stage0Data.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Stage0의 갤러리 미니사이즈 사진들을 저장하는곳. 
@@ -9,6 +9,41 @@ using UnityEngine;
 /// </summary>
 public class Stage0Data : DontDestroy<Stage0Data>
 {
-    public Texture squirrel;
+    public SpriteRenderer squirrel;
     public Sprite sprite;
+
+    public void SetSprite(Sprite img)
+    {
+        sprite = img;
+    }
+
+
+    #region 인스펙터에서 안보이는 PlayerData 내부의 정보 확인용. 디버깅용.
+    void OnEnable()
+    {
+        // 씬 매니저의 sceneLoaded에 체인을 건다.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    // 체인을 걸어서 이 함수는 매 씬마다 호출된다.
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //Debug.Log(npa_Stone_ActiveSelf.Length);
+        //for (int i = 0; i < npa_Stone_ActiveSelf.Length; i++)
+        //{
+        //    Debug.Log(i+", "+npa_Stone_ActiveSelf[i]);
+        //}
+        if (SceneManager.GetActiveScene().name.Contains("Gallery0_0"))
+        {
+            squirrel = GameObject.Find("Test").GetComponent<SpriteRenderer>();
+            squirrel.sprite = sprite;
+        }
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    #endregion
 }
