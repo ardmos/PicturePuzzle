3213ee58d11f6c3e05f8301f2f03621a2374fceb
ardmos <46891528+ardmos@users.noreply.github.com>
2021-07-04 21:47:07 +0900

    캡쳐만드는중

---
+++ PicturePuzzle/Assets/Scripts/Data/Stage0/Stage0Data.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Stage0의 갤러리 미니사이즈 사진들을 저장하는곳. 
+/// 
+/// </summary>
+public class Stage0Data : DontDestroy<Stage0Data>
+{
+    public Texture squirrel;
+    public Sprite sprite;
+}
